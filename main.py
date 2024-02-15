306b765cb4cd63b6473f85dbf6658a22f2337d75
karga-tr <al_dokan_20@hotmail.com>
2024-02-15 22:12:50 +0300

    Created Main class.

--- main.py
+++ main.py
@@ -4,28 +4,34 @@ from sys import exit
 from game import Game 
 from menu import Menu 
 
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
+class Main():
+    def __init__(self):
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        self.clock = pygame.time.Clock()
 
-clock = pygame.time.Clock()
+        self.game = Game()
+        self.menu = Menu()
 
-game = Game()
-menu = Menu()
+    def run(self):
+        while True:
+            self.clock.tick(FPS)
 
-while True:
-    clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                    
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.menu.gameOn = False 
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-            
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                menu.gameOn = False 
+            self.screen.fill('black')
+            if self.menu.gameOn:
+                self.game.run()
+            else:
+                self.menu.run()
+            pygame.display.update()
 
-    screen.fill('black')
-    if menu.gameOn:
-        game.run()
-    else:
-        menu.run()
-    pygame.display.update()
+if __name__ == "__main__":
+    main = Main()
+    main.run()
